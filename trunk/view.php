ec005a56af42125aea19691d7fa33f7c446e1180
dfrese <dfrese@8440c51d-cdff-0310-b74f-a9601ebbeab2>
2005-10-28 21:22:08 +0000

    Subject: Implemented basic authentication support

--- trunk/view.php
+++ trunk/view.php
@@ -27,15 +27,21 @@ require_once('src/utils.inc');
 define_globals(0);
 
 require_once('src/resources.inc');
+require_once('src/authenticate.inc');
 
 function view($location, $args) {
   $resource = get_resource($location, $args);
+
+  verify_view_authorization($resource);
+
   global $xeemes_current_resource;
   $xeemes_current_resource = $resource;
   if (($resource->type() & XEEMES_RESOURCE_XML) > 0) {
     if (!$resource->exists()) {
+      header('HTTP/1.1 404 Not found');
       print('<html><body>404 - not found</body></html>'); // TODO
     } else {
+      header('Content-Type: text/html; charset=utf-8');
       print($resource->xmlContent());
     }
   } else {
@@ -48,6 +54,6 @@ function view($location, $args) {
   }
 }
 
-view($_SERVER['PATH_INFO'], $_SERVER['argv']);
+view($_SERVER['PATH_INFO'], $_GET);
 
 ?>
